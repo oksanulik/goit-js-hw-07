f43836b7163152212a9e33a7edc2c5e3954775c2
oksanulik <kalko_oksanka@ukr.net>
2023-06-06 00:41:12 +0300

    1fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const imageMarkup = createGalleryImage(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", imageMarkup);
+
+function createGalleryImage(items) {
+  return galleryItems
+    .map((item) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              data-source="${item.original}"
+              alt="${item.description}"
+            />
+          </a>
+        </li>`;
+    })
+    .join("");
+}
+console.log(galleryEl);
+//
+createGalleryImage();
+console.log(createGalleryImage());
