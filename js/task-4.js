df290c1efe0a977f947cd80d598e87a2f1df203a
OksanaT <kalko_oksanka@ukr.net>
2024-10-18 22:49:34 +0300

    fix4

--- js/task-4.js
+++ js/task-4.js
@@ -6,16 +6,17 @@ function loginForm(event) {
   event.preventDefault();
 
   const elements = event.currentTarget;
-  const elementsEmail = elements.email.value;
-  const elementsPassword = elements.password.value;
+  const elementsEmail = elements.email.value.trim();
+  const elementsPassword = elements.password.value.trim();
 
   if (elementsEmail === "" || elementsPassword === "") {
     alert("All form fields must be filled in");
+    return;
   }
 
   const info = {
-    email: elements.email.value.trim(),
-    password: elements.password.value.trim(),
+    email: elementsEmail,
+    password: elementsPassword,
   };
   console.log(info);
 
